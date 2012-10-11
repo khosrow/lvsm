3964a87e39dbda8cdccad9e0a8f69903b329cf89
Khosrow E <khosrow.ebrahimpour@gmail.com>
2012-10-10 22:25:35 -0400

    fixed file perms

--- lvsm/test/test_status.py
+++ lvsm/test/test_status.py
@@ -10,18 +10,20 @@ import lvsm
 
 
 class TestStatusModule(unittest.TestCase):
-    config = {'director_config': './etc/ldirectord.conf',
+    config = {'ipvsadm': './scripts/ipvsadm',
+              'iptables': './scripts/iptables',
+              'director_config': './etc/ldirectord.conf',
               'firewall_config': './etc/iptables.rules',
               'dsh_group': '',
               'director': 'ldirectord',
               'maintenance_dir': ''
               }
     shell = lvsm.StatusPrompt(config)
-
+  
     def test_showdirector(self):
       output = StringIO.StringIO()
       sys.stdout = output
-      expected_result = "# director config\n"
+      expected_result = "This is ipvsadm\n"
       self.shell.onecmd(' show director')
       result = output.getvalue()
       self.assertEqual(result, expected_result)
