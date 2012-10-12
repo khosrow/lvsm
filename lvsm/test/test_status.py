c0cb2fa46af7f8c4e1ccc6bcff876bddce12c9b3
Khosrow E <khosrow.ebrahimpour@gmail.com>
2012-10-11 21:10:26 -0400

    Updated test suite

--- lvsm/test/test_status.py
+++ lvsm/test/test_status.py
@@ -10,20 +10,20 @@ import lvsm
 
 
 class TestStatusModule(unittest.TestCase):
-    config = {'ipvsadm': './scripts/ipvsadm',
-              'iptables': './scripts/iptables',
-              'director_config': './etc/ldirectord.conf',
-              'firewall_config': './etc/iptables.rules',
-              'dsh_group': '',
-              'director': 'ldirectord',
-              'maintenance_dir': ''
-              }
-    shell = lvsm.StatusPrompt(config)
-  
-    def test_showdirector(self):
-      output = StringIO.StringIO()
-      sys.stdout = output
-      expected_result = """IP Virtual Server version 1.2.1 (size=4096)
+  config = {'ipvsadm': './scripts/ipvsadm',
+            'iptables': './scripts/iptables',
+            'director_config': './etc/ldirectord.conf',
+            'firewall_config': './etc/iptables.rules',
+            'dsh_group': '',
+            'director': 'ldirectord',
+            'maintenance_dir': ''
+            }
+  shell = lvsm.StatusPrompt(config)
+
+  def test_showdirector(self):
+    output = StringIO.StringIO()
+    sys.stdout = output
+    expected_result = """IP Virtual Server version 1.2.1 (size=4096)
 Prot LocalAddress:Port Scheduler Flags
   -> RemoteAddress:Port           Forward Weight ActiveConn InActConn
 TCP  lvs-test-web1.cmc.ec.gc.ca:w rr
@@ -43,14 +43,14 @@ TCP  lvs-test-web2.cmc.ec.gc.ca:w rr
   -> lvs-test-fe02:www            Masq    1      0          0         
 UDP  lvs-test-other.cmc.ec.gc.ca: rr
   -> lvs-test-fe02:domain         Masq    1      0          0\n"""
-      self.shell.onecmd(' show director')
-      result = output.getvalue()
-      self.assertEqual(result.rstrip(), expected_result.rstrip())
+    self.shell.onecmd(' show director')
+    result = output.getvalue()
+    self.assertEqual(result.rstrip(), expected_result.rstrip())
 
-    def test_showfirewall(self):
-      output = StringIO.StringIO()
-      sys.stdout = output
-      expected_result = """Chain INPUT (policy ACCEPT)
+  def test_showfirewall(self):
+    output = StringIO.StringIO()
+    sys.stdout = output
+    expected_result = """Chain INPUT (policy ACCEPT)
 target     prot opt source               destination         
 
 Chain FORWARD (policy ACCEPT)
@@ -58,6 +58,31 @@ target     prot opt source               destination
 
 Chain OUTPUT (policy ACCEPT)
 target     prot opt source               destination\n"""
-      self.shell.onecmd(' show firewall')
-      result = output.getvalue()
-      self.assertEqual(result.rstrip(), expected_result.rstrip())
+    self.shell.onecmd(' show firewall')
+    result = output.getvalue()
+    self.assertEqual(result.rstrip(), expected_result.rstrip())
+
+  def test_showvirtualtcp(self):
+    output = StringIO.StringIO()
+    sys.stdout = output
+    expected_result = """IP Virtual Server version 1.2.1 (size=4096)
+Prot LocalAddress:Port Scheduler Flags
+  -> RemoteAddress:Port           Forward Weight ActiveConn InActConn
+TCP  lvs-test-web1.cmc.ec.gc.ca:w rr
+  -> lvs-test-fe02:www            Masq    1      0          0         
+  -> lvs-test-fe03:www            Masq    1      0          0"""
+    self.shell.onecmd(' show virtual tcp lvs-test-web1 80')
+    result = output.getvalue()
+    self.assertEqual(result.rstrip(), expected_result.rstrip())
+
+  def test_showvirtualudp(self):
+    output = StringIO.StringIO()
+    sys.stdout = output
+    expected_result = """IP Virtual Server version 1.2.1 (size=4096)
+Prot LocalAddress:Port Scheduler Flags
+  -> RemoteAddress:Port           Forward Weight ActiveConn InActConn
+UDP  lvs-test-other.cmc.ec.gc.ca: rr
+  -> lvs-test-fe02:domain         Masq    1      0          0"""
+    self.shell.onecmd(' show virtual udp lvs-test-other 53')
+    result = output.getvalue()
+    self.assertEqual(result.rstrip(), expected_result.rstrip())
