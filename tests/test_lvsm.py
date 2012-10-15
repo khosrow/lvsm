ded6b7262cafd538541b10176cbeaddd48d654ab
Khosrow E <khosrow.ebrahimpour@gmail.com>
2012-10-15 18:56:05 +0000

    consolidated test suite for lvsm.py file.

--- tests/test_lvsm.py
+++ tests/test_lvsm.py
@@ -8,6 +8,58 @@ from lvsm import lvsm
 path = os.path.abspath(os.path.dirname(__file__))
 
 
+class TestConfig(unittest.TestCase):
+    """Verify correct functionality in configure"""
+    config = {'director_config': path + '/etc/ldirectord.conf',
+              'firewall_config': path + '/etc/iptables.rules',
+              'dsh_group': '',
+              'director': 'ldirectord',
+              'maintenance_dir': ''
+              }
+    shell = lvsm.ConfigurePrompt(config)
+
+    def test_showdirector(self):
+        output = StringIO.StringIO()
+        sys.stdout = output
+        expected_result = "# director config\n"
+        self.shell.onecmd(' show director')
+        result = output.getvalue()
+        self.assertEqual(result, expected_result)
+
+    def test_showfirewall(self):
+        output = StringIO.StringIO()
+        sys.stdout = output
+        expected_result = "# iptables\n"
+        self.shell.onecmd(' show firewall')
+        result = output.getvalue()
+        self.assertEqual(result, expected_result)
+
+    # def test_sync(self):
+    #   output = StringIO.StringIO()
+    #   sys.stdout = output
+    #   expected_result = ""
+    #   self.shell.onecmd(' sync')
+    #   result = output.getvalue()
+    #   self.assertEqual(result, expected_result)
+
+    # def test_editdirector(self):
+    #   output = StringIO.StringIO()
+    #   sys.stdout = output
+    #   expected_result = ""
+    #   self.shell.onecmd(' edit director')
+    #   result = output.getvalue()
+    #   self.assertEqual(result, expected_result)
+
+
+class TestConfigErrors(unittest.TestCase):
+    """Verify error checking in configure"""
+    def test_showdirector(self):
+        self.assertTrue(True)
+
+    def test_showfirewall(self):
+        self.assertTrue(True)
+
+
 class TestStatus(unittest.TestCase):
     config = {'ipvsadm': path + '/scripts/ipvsadm',
               'iptables': path + '/scripts/iptables',
@@ -110,4 +162,12 @@ TCP  lvs-test-web2.cmc.ec.gc.ca:w rr
         self.assertTrue(True)
 
     def test_enablereal(self):
+        self.assertTrue(True)
+
+
+class TestStatusErrors(unittest.TestCase):
+    def test_showdirector(self):
+        self.assertTrue(True)
+
+    def test_showfirewall(self):
         self.assertTrue(True)
