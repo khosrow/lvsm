13106c1fa974611585f082ebc473e5c0f59182e2
Khosrow E <khosrow@khosrow.ca>
2012-11-24 13:00:38 -0500

    Fix for issue #11

--- lvsm/firewall.py
+++ lvsm/firewall.py
@@ -24,13 +24,15 @@ class Firewall():
             print stderr
         return False
 
-    def show_virtual(self, host, numeric):
+    def show_virtual(self, host, port, numeric):
         args = self.iptables + ' -L INPUT'
         if numeric:
             args = args + ' -n'
             hostname = utils.gethostname(host)
+            portname = utils.getportnum(port)
         else:
             hostname = host
+            portname = port
         try:
             proc = subprocess.Popen(args, stdout=subprocess.PIPE, shell=True)
         except OSError as e:
@@ -40,8 +42,15 @@ class Firewall():
         if stdout:
             lines = stdout.split('\n')
             for line in lines:
-                if line.find(" " + hostname) > -1:
-                    print line
+                # break the iptables output into tokens
+                # assumptions:
+                # 5th item is the hostname
+                # 8th item is the portname
+                tokens = line.split()
+                if len(tokens) >= 7:
+                    if (tokens[4] == hostname and
+                        tokens[6] == "dpt:" + portname):
+                        print line
             return True
         elif stderr:
             print stderr
