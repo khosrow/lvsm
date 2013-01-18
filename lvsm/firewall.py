d90f4abd2c4229f7ea1c4a76738a56d148f60231
Khosrow E <khosrow@khosrow.ca>
2013-01-18 22:24:57 +0000

    output functions now return list() instead of bool

--- lvsm/firewall.py
+++ lvsm/firewall.py
@@ -8,39 +8,33 @@ class Firewall():
         self.iptables = iptables
 
     def show(self, numeric):
-        args = self.iptables + ' -L -v'
+        args = [self.iptables, "-L", "-v"]
         if numeric:
-            args = args + ' -n'
+            args.append("-n")
         try:
-            proc = subprocess.Popen(args, stdout=subprocess.PIPE, shell=True)
+            output = subprocess.check_output(args)
         except OSError as e:
             print "[ERROR] problem with iptables - " + e.strerror
-            return False
-        stdout, stderr = proc.communicate()
-        if stdout:
-            print stdout
-            return True
-        elif stderr:
-            print stderr
-        return False
+            return
+        return output.split("\n")
 
     def show_virtual(self, host, port, numeric):
-        args = self.iptables + ' -L INPUT'
+        result = list()
+        args = [self.iptables, '-L', 'INPUT']
         if numeric:
-            args = args + ' -n'
+            args.append('-n')
             hostname = utils.gethostname(host)
             portname = utils.getportnum(port)
         else:
             hostname = host
             portname = port
         try:
-            proc = subprocess.Popen(args, stdout=subprocess.PIPE, shell=True)
+            output = subprocess.check_output(args)
         except OSError as e:
             print "[ERROR] problem with iptables - " + e.strerror
-            return False
-        stdout, stderr = proc.communicate()
-        if stdout:
-            lines = stdout.split('\n')
+            return
+        if output:            
+            lines = output.split('\n')
             for line in lines:
                 # break the iptables output into tokens
                 # assumptions:
@@ -49,9 +43,6 @@ class Firewall():
                 tokens = line.split()
                 if len(tokens) >= 7:
                     if (tokens[4] == hostname and
-                        tokens[6] == "dpt:" + portname):
-                        print line
-            return True
-        elif stderr:
-            print stderr
-        return False
+                        tokens[6] == "dpt:" + portname):                        
+                        result.append(line)
+        return result
