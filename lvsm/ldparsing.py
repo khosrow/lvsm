dfc29657ee1922d4ef163781483368b3358ee4b0
Khosrow E <khosrow@khosrow.ca>
2013-07-01 16:22:10 -0400

    cleaned up parser file. Needs to be integerated into module

--- lvsm/ldparsing.py
+++ lvsm/ldparsing.py
@@ -1,9 +1,14 @@
+import logging
 from pyparsing import *
+
 
 # Helper function to validate IPv4 addresses
 def validate_ip4(s, loc, tokens):
     octets = tokens[0].split(".")
-    print octets
+
+    logger = logging.getLogger('ldparser')
+    logger.debug('IP4 octets: %s', octets)
+
     for octet in octets:
         if int(octet) <= 255 and int(octet) >= 0:
             pass
@@ -14,7 +19,8 @@ def validate_ip4(s, loc, tokens):
 
 # Helper function that verifies we have a valid port number
 def validate_port(s, loc, tokens):
-    port = tokens[1]
+    # port = tokens[1]
+    port = tokens[0]
     if int(port) < 65535 and int(port) > 0:
         return tokens
     else:
@@ -23,7 +29,10 @@ def validate_port(s, loc, tokens):
         
 def validate_scheduler(s, loc, tokens):
     schedulers = ['rr', 'wrr', 'lc', 'wlc', 'lblc', 'lblcr', 'dh', 'sh', 'sed', 'nq']
-    print tokens
+
+    logger = logging.getLogger('ldparser')
+    logger.debug('Tokens: %s', tokens)
+
     if tokens[0][1] in schedulers:
         return tokens
     else:
@@ -39,10 +48,11 @@ def validate_checktype(s, loc, tokens):
         raise ParseFatalException(s, loc, errmsg)
 
 def validate_int(s, loc, tokens):
-    if tokens[0][1].isdigit() and int(tokens[0][1]) > 0:
+    # if tokens[0][1].isdigit() and int(tokens[0][1]) > 0:
+    if tokens[0].isdigit() and int(tokens[0]) > 0:
         return tokens
     else:
-        errmsg = "Invalid value for " + tokens[0][0] + "!"
+        errmsg = "Value must be an integer!"
         raise ParseFatalException(s, loc, errmsg)
 
 def validate_protocol(s, loc, tokens):
@@ -63,11 +73,18 @@ def validate_service(s, loc, tokens):
         errmsg = "Invalid service type!"
         raise ParseFatalException(s, loc, errmsg)
 
-def validate_yesno(s, loc, errmsg):
-    if tokens[0][1] == "yes" or tokens[0][1] == "no":
+def validate_yesno(s, loc, tokens):
+    if tokens[0] == "yes" or tokens[0] == "no":
         return tokens
     else:
         errmsg = "Value must be 'yes' or 'no'"
+        raise ParseFatalException(s, loc, errmsg)
+
+def validate_httpmethod(s, loc, tokens):
+    if tokens[0] in ['GET', 'HEAD']:
+        return tokens
+    else:
+        errmsg = "Value must be 'GET' or 'HEAD'"
         raise ParseFatalException(s, loc, errmsg)
 
 def parse_config(configfile):
@@ -78,17 +95,24 @@ def parse_config(configfile):
     COLON = Literal(":").suppress()
     # INDENT = White("    ").suppress()
     INDENT = Regex("^ {4,}").suppress()
-    
+
     ## Define parsing rules for single lines
     hostname = Word(alphanums + '._+-')
     ip4_address = Combine(Word(nums) - ('.' + Word(nums)) * 3)
-    # Validate ip4 addresses
     ip4_address.setParseAction(validate_ip4)
-    ip4_addrport = (ip4_address | hostname) + COLON + (Word(nums) | Word(alphas))
+
+    port = Word(alphanums)
+    port.setParseAction(validate_port)
+
+    ip4_addrport = (ip4_address | hostname) + COLON + port
     # Validate port numbers
-    ip4_addrport.setParseAction(validate_port)
+    # ip4_addrport.setParseAction(validate_port)
 
     yesno = Literal("yes") | Literal("no")
+    yesno.setParseAction(validate_yesno)
+
+    integer = Word(printables)
+    integer.setParseAction(validate_int)
 
     real4 = Group(Literal("real") + EQUAL + ip4_addrport + Optional(Word(nums)))
     virtual4 = Group(Literal("virtual") + EQUAL + ip4_addrport)
@@ -99,64 +123,54 @@ def parse_config(configfile):
     autoreload = Group(Literal("autoreload") + EQUAL + yesno)
     callback = Group(Literal("callback") + EQUAL + dblQuotedString)
     checkcommand = Group(Literal("checkcommand") + EQUAL + (dblQuotedString | Word(printables)))
-    checkinterval = Group(Literal("checkinterval") + EQUAL + Word(alphanums))
-    checktimeout = Group(Literal("checktimeout") + EQUAL + Word(alphanums))
+    # checkinterval = Group(Literal("checkinterval") + EQUAL + Word(alphanums))
+    checkinterval = Group(Literal("checkinterval") + EQUAL + integer)
+    checktimeout = Group(Literal("checktimeout") + EQUAL + integer)
     checktype = Group(Literal("checktype") + EQUAL + Word(alphanums))
     checkport = Group(Literal("checkport") + EQUAL + Word(alphanums))
-    cleanstop = Group(Literal("cleanstop") + EQUAL + Word(alphas))
+    cleanstop = Group(Literal("cleanstop") + EQUAL + yesno)
     database = Group(Literal("database") + EQUAL + dblQuotedString)
     emailalert = Group(Literal("emailalert") + EQUAL + Word(printables))
-    emailalertfreq = Group(Literal("emailalertfreq") + EQUAL + Word(nums))
+    emailalertfreq = Group(Literal("emailalertfreq") + EQUAL + integer)
+    emailalertfrom = Group(Literal("emailalertfrom") + EQUAL + Word(printables))
     emailalertstatus = Group(Literal("emailalertstatus") + EQUAL + Word(printables))
     execute = Group(Literal("execute") + EQUAL + Word(printables))
-    failurecount = Group(Literal("failurecount") + EQUAL + Word(alphanums))
+    failurecount = Group(Literal("failurecount") + EQUAL + integer)
     fallback = Group(Literal("fallback") + EQUAL + ip4_addrport)
     fallbackcommand = Group(Literal("fallbackcommand") + EQUAL + (dblQuotedString | Word(printables)))
-    fork = Group(Literal("fork") + EQUAL + Word(alphas))
+    fork = Group(Literal("fork") + EQUAL + yesno)
     httpmethod = Group(Literal("httpmethod") + EQUAL + Word(alphanums))
     load = Group(Literal("load") + EQUAL + dblQuotedString)
     logfile = Group(Literal("logfile") + EQUAL + dblQuotedString)
     login = Group(Literal("login") + EQUAL + dblQuotedString)
+    maintenance_dir = Group(Literal("maintenance_dir") + EQUAL + Word(printables))
     monitorfile = Group(Literal("monitorfile") + EQUAL + (dblQuotedString | Word(printables)))
-    negotiatetimeout = Group(Literal("negotiatetimeout") + EQUAL + Word(alphanums))
+    negotiatetimeout = Group(Literal("negotiatetimeout") + EQUAL + integer)
     netmask = Group(Literal("netmask") + EQUAL + ip4_address)
     passwd = Group(Literal("passwd") + EQUAL + dblQuotedString)
-    persistent = Group(Literal("persistent") + EQUAL + Word(alphanums))
+    persistent = Group(Literal("persistent") + EQUAL + integer)
     protocol = Group(Literal("protocol") + EQUAL + Word(alphas))
-    quiescent = Group(Literal("quiescent") + EQUAL + Word(alphas))
+    quiescent = Group(Literal("quiescent") + EQUAL + yesno)
+    readdquiescent = Group(Literal("readdquiescent") + EQUAL + yesno)
     receive = Group(Literal("receive") + EQUAL + dblQuotedString)
     request = Group(Literal("request") + EQUAL + dblQuotedString)
     scheduler = Group(Literal("scheduler") + EQUAL + Word(alphas))
     secret = Group(Literal("secret") + EQUAL + dblQuotedString)
     service = Group(Literal("service") + EQUAL + Word(alphas))
+    supervised = Group(Literal("supervised") + EQUAL + yesno)
     smtp = Group(Literal("smtp") + EQUAL + (ip4_address | hostname))
     virtualhost = Group(Literal("virtualhost") + EQUAL + hostname )
 
-    # TODO: fork was the last one I did
-
     # Validate all the matched elements
-    # autoreload.setParseAction()
-    checkinterval.setParseAction(validate_int)
     checkport.setParseAction(validate_port)
     checktype.setParseAction(validate_checktype)
-    checktimeout.setParseAction(validate_int)
-    # cleanstop.setParseAction()
-    # emailalert.setParseAction()
-    # emailalertfreq.setParseAction()
-    # emailalertstatus.setParseAction()
-    failurecount.setParseAction(validate_int)
-    # fork.setParseAction()
-    # httpmethod.setParseAction()
-    # monitorfile.setParseAction()
-    negotiatetimeout.setParseAction(validate_int)
-    persistent.setParseAction(validate_int)
+    httpmethod.setParseAction(validate_httpmethod)
     protocol.setParseAction(validate_protocol)
-    # quiescent.setParseAction()
     scheduler.setParseAction(validate_scheduler)
     service.setParseAction(validate_service)
-    # smtp.setParseAction()
 
-    # TODO: validate protocol=
+    # TODO: validate protocol with respect to the virtual directive
+    # TODO: implement virtual6, real6, fallback6
 
     optionals << ( checkcommand | checkinterval | checktimeout | checktype | checkport | cleanstop
                 | database | emailalert | emailalertfreq | emailalertstatus | failurecount | fallback
@@ -169,7 +183,10 @@ def parse_config(configfile):
     #             | passwd | persistent | protocol | quiescent | receive | request | scheduler | secret
     #             | service | smtp | virtualhost)
 
-    glb_optionals = ( checktimeout | negotiatetimeout | checkinterval | failurecount | autoreload)
+    glb_optionals = ( checktimeout | negotiatetimeout | checkinterval | failurecount | fallback
+                    | fallbackcommand | autoreload | callback | logfile | execute | fork | supervised
+                    | quiescent | readdquiescent | emailalert | emailalertfreq | emailalertstatus
+                    | emailalertfrom | cleanstop | smtp | maintenance_dir )
 
     ## Define block of config
     # both of the next two styles works
@@ -184,27 +201,24 @@ def parse_config(configfile):
     allconfig.ignore(comment)
 
     try:
-        # tokens = virtual4_block.parseString(configfile)
         tokens = allconfig.parseString(configfile)
     except ParseException as pe:
+        print "[ERROR] While parsing config file"
         print pe
     except ParseFatalException as pe:
+        print "[ERROR] While parsing config file"
         print pe
     else:
         return tokens
     finally:
-        # print "contents"
-        import pprint
-        # pprint.pprint(tokens.asList())
+        pass
 
+FORMAT = "%(asctime)s %(name)s[%(levelname)s][%(funcName)s]: %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.DEBUG, datefmt='%b %d %H:%m:%S')
+logger = logging.getLogger('ldparser')
 conf = "".join(open("./test.cfg").readlines())
 config = parse_config(conf)
 
 if config:
-    print "== Dict == "
-    for item in config.asDict():
-        print item
-    print "== List =="
     for item in config.asList():
-        print item
-
+        logger.debug('Token: %s' ,item)
