267096aabea4bb8bc20b9dc3c0141d8cf68dc90b
Khosrow E <khosrow@khosrow.ca>
2013-11-16 19:56:00 -0500

    Fixed imports for parse testing script

--- lvsm/modules/parse_test.py
+++ lvsm/modules/parse_test.py
@@ -1,6 +1,7 @@
-import ldparser
-import kaparser
 import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from lvsm.modules import ldparser, kaparser
 
 if len(sys.argv) != 2:
 	print "Need config file name"
