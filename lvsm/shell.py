ed6964451773690fbe8a2d9c87a7e5091863f327
Khosrow E <khosrow@khosrow.ca>
2013-12-10 21:32:20 -0500

    Added disable/enable to virtual shell

--- lvsm/shell.py
+++ lvsm/shell.py
@@ -577,6 +577,58 @@ class VirtualPrompt(CommandPrompt):
         else:
             print syntax
 
+    def do_disable(self, line):
+        """
+        \rDisable real server across VIPs.
+        \rsyntax: disable <rip> <port>
+        """
+
+        syntax = "syntax: disable <rip> <port>"
+
+        commands = line.split()
+        if len(commands) > 2 or len(commands) == 0:
+            print syntax
+        elif len(commands) <= 2:
+            host = commands[0]
+            if len(commands) == 1:
+                port = ''
+            elif len(commands) == 2:
+                port = commands[1]
+            else:
+                print syntax
+                return
+            # ask for an optional reason for disabling
+            reason = raw_input("Reason for disabling [default = None]: ")
+            if not self.director.disable(host, port, reason):
+                logger.error("Could not disable %s" % host)
+        else:
+            print syntax
+
+    def do_enable(self, line):
+        """
+        \rEnable real server across VIPs.
+        \rsyntax: enable <rip> <port>
+        """
+
+        syntax = "syntax: enable <rip> <port>"
+
+        commands = line.split()
+        if len(commands) > 2 or len(commands) == 0:
+            print syntax
+        elif len(commands) <= 2:
+            host = commands[0]
+            if len(commands) == 1:
+                port = ''
+            elif len(commands) == 2:
+                port = commands[0]
+            else:
+                print syntax
+                return
+            if not self.director.enable(host, port):
+                logger.error("Could not enable %s" % host)
+        else:
+            print syntax
+
     def complete_show(self, text, line, begidx, endidx):
         """Tab completion for the show command"""
         if len(line) < 8:
