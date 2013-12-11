23d3dda9dbfe213acbf098a290417091e2766829
Khosrow E <khosrow@khosrow.ca>
2013-12-10 21:33:08 -0500

    Commenting out enable/disable. To be used from parent class

--- lvsm/modules/keepalivedprompts.py
+++ lvsm/modules/keepalivedprompts.py
@@ -8,21 +8,40 @@ class VirtualPrompt(shell.VirtualPrompt):
     def __init__(self, config, stdin=sys.stdin, stdout=sys.stdout):
         shell.VirtualPrompt.__init__(self, config)
 
-    def do_disable(self, line):
-        """
-        \rDisable real server belonging to a VIP.
-        \rsyntax: disable tcp|udp|fwm <vip> <port> real <real> <port>
-        """
+    # def do_disable(self, line):
+    #     """
+    #     \rDisable real server belonging to a VIP.
+    #     \rsyntax: disable tcp|udp|fwm <vip> <port> real <real> <port>
+    #     """
 
-        syntax = "syntax: disable tcp|udp|fwm <vip> <port> real <real> <port>"
+    #     syntax = "syntax: disable tcp|udp|fwm <vip> <port> real <real> <port>"
 
-        commands = line.split()
-        if len(commands) == 6:
-            module = commands[0]
-            vip = commands[1]
-            vport = commands[2]
-            rip = commands[4]
-            rport = commands[5]
-            print "Disabling real: %s:%s" % (rip, rport)
-        else:
-            print "\n%s\n" % syntax
+    #     commands = line.split()
+    #     if len(commands) == 6:
+    #         module = commands[0]
+    #         vip = commands[1]
+    #         vport = commands[2]
+    #         rip = commands[4]
+    #         rport = commands[5]
+    #         print "Disabling real: %s:%s" % (rip, rport)
+    #     else:
+    #         print "\n%s\n" % syntax
+
+    # def do_enable(self, line):
+    #     """
+    #     \renable real server belonging to a VIP.
+    #     \rsyntax: enable tcp|udp|fwm <vip> <port> real <real> <port>
+    #     """
+
+    #     syntax = "syntax: enable tcp|udp|fwm <vip> <port> real <real> <port>"
+
+    #     commands = line.split()
+    #     if len(commands) == 6:
+    #         module = commands[0]
+    #         vip = commands[1]
+    #         vport = commands[2]
+    #         rip = commands[4]
+    #         rport = commands[5]
+    #         print "enabling real: %s:%s" % (rip, rport)
+    #     else:
+    #         print "\n%s\n" % syntax
