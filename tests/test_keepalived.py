b333fa18c6568ad872bd31a53a2381f531775f86
Khosrow E <khosrow@khosrow.ca>
2013-12-20 14:42:29 +0000

    added keepalived test cases

---
+++ tests/test_keepalived.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+import sys
+import StringIO
+
+path = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../lvsm')))
+
+
+from lvsm.modules import keepalived
+
+
+class Keepalived(unittest.TestCase):
+    """Tests for the functionality of the keepalived module"""
+    def setUp(self):
+        self.director = keepalived.Keepalived(path + '/scripts/ipvsadm',
+                                              path + '/etc/keepalived.conf')
+
+    def test_disablehost(self):
+        self.assertTrue(True) 
+    
+    def test_disablehostport(self):
+        self.assertTrue(True)
+
+    def test_parseconfig1(self):
+        # Testing parser on a valid config file
+        configfile = path + '/etc/keepalived.conf'
+        self.assertTrue(self.director.parse_config(configfile))
+
+    def test_parseconfig2(self):
+        # Testing parser on an invalid config file
+        configfile = path + '/etc/keepalived.conf-bad'
+        self.assertFalse(self.director.parse_config(configfile))
+
+if __name__ == "__main__":
+    unittest.main()
