ac3fcce770546365c65b37679c580637c5ddcd4b
Khosrow E <khosrow@khosrow.ca>
2014-01-27 02:47:38 +0000

    updating dummy module

--- lvsm/snimpy_dummy/manager.py
+++ lvsm/snimpy_dummy/manager.py
@@ -2,6 +2,14 @@
 Dummy module that simulates snimpy functionality on Keepalived 
 for unit testing
 """
+# The following vip/rip are hardcoded into the dummy module
+# TCP  82.94.164.162:80
+#   -> 216.34.181.45:80
+#   -> 173.194.43.3:80         
+# UDP  82.94.164.162:53
+#   -> 208.67.222.222:53
+#   -> 208.67.220.220:53
+import socket
 
 class Manager(object):
     def __init__(self,
@@ -18,9 +26,12 @@ class Manager(object):
                  privprotocol=None,
                  privpassword=None):
         """dummy init method"""
-        self.virtualServerAddress = list()
-        self.virtualServerPort = list()
-        self.virtualServerRealServersTotal = list()
+        self.virtualServerAddress = [socket.inet_aton('82.94.164.162'),
+                                     socket.inet_aton('82.94.164.162')]
+        self.virtualServerRealServersTotal = [2, 2]
+        self.virtualServerPort = [80, 53]
+        self.realServerAddress = []
+        self.realServerWeight = []
 
 
 def load(mibname):
