46671558497842ee3d3b1ac201c951b006c97fd8
Khosrow E <khosrow@khosrow.ca>
2014-02-22 19:46:50 -0500

    Added exception class

--- lvsm/snimpy_dummy/mib.py
+++ lvsm/snimpy_dummy/mib.py
@@ -5,3 +5,8 @@ Dummy module to simulate the mib module in snimpy
 def load(mib):
 	"""dummy load"""
 	pass
+
+class SMIException(Exception):
+    """SMI related exception. Any exception thrown in this module is
+    inherited from this one."""
+    pass
