12469b43a108073f80d52b38ac1287561f175673
Khosrow E <khosrow@khosrow.ca>
2014-03-13 02:52:44 +0000

    Added real server shell level to each director.

--- lvsm/modules/keepalivedprompts.py
+++ lvsm/modules/keepalivedprompts.py
@@ -1,9 +1,12 @@
+import logging
 import sys
 from lvsm import shell
 
+logger = logging.getLogger('lvsm')
+
 class VirtualPrompt(shell.VirtualPrompt):
     """
-    Display information about virtual IP with ldirector specific fucntions.
+    Display information about virtual IP with keepalived specific functions.
     """
     def __init__(self, config, stdin=sys.stdin, stdout=sys.stdout):
         shell.VirtualPrompt.__init__(self, config)
@@ -45,3 +48,141 @@ class VirtualPrompt(shell.VirtualPrompt):
     #         print "enabling real: %s:%s" % (rip, rport)
     #     else:
     #         print "\n%s\n" % syntax
+
+class RealPrompt(shell.RealPrompt):
+    """
+    Display information about real servers with keepalived specific functions.
+    """
+    def __init__(self, config, rawprompt='', stdin=sys.stdin, stdout=sys.stdout):
+        shell.RealPrompt.__init__(self, config)
+
+    def do_disable(self, line):
+        """
+        \rDisable real server across VIPs.
+        \rsyntax: disable tcp|udp|fwm <rip> [<port>] [<vip> <port>]
+        """
+
+        syntax = "*** Syntax: disable tcp|udp|fwm <rip> [<port>] [<vip> <port>]"
+
+        # Some default values to be used
+        port = ''
+        vip = ''
+        vport = ''
+        commands = line.split()
+        if len(commands) < 2 or len(commands) > 5:
+            print syntax
+        elif len(commands) >= 2:
+            protocol = commands[0]
+            # Verify protocol is valid
+            if protocol not in self.protocols:
+                print syntax 
+                return 
+
+            host = commands[1]
+            if len(commands) == 3:
+                port = commands[2]
+                if len(commands) == 4:
+                    vip = commands[3]
+                    if len(commands) == 5:
+                        vport = commands[4]
+
+            # ask for an optional reason for disabling
+            reason = raw_input("Reason for disabling [default = None]: ")
+            # if not self.director.disable(host, port, reason=reason):
+            if not self.director.disable(protocol,
+                                         host, port,
+                                         vip, vport,
+                                         reason=reason):
+                logger.error("Could not disable %s" % host)
+        else:
+            print syntax
+
+    def do_enable(self, line):
+        """
+        \rEnable real server across VIPs.
+        \rsyntax: enable tcp|udp|fwm <rip> [<port>] [<vip> <port>]
+        """
+
+        syntax = "*** Syntax: enable tcp|udp|fwm <rip> [<port>] [<vip> <port>]"
+
+        # Some default values to be used
+        port = ''
+        vip = ''
+        vport = ''
+
+        commands = line.split()
+        if len(commands) < 2 or len(commands) > 5:
+            print syntax
+        elif len(commands) >= 2:
+            protocol = commands[0]
+            # Verify protocol is valid
+            if protocol not in self.protocols:
+                print syntax
+                return
+
+            host = commands[1]
+            if len(commands) == 3:
+                port = commands[2]
+            elif len(commands) == 4:
+                vip = commands[3]
+                if len(commands) == 5:
+                    vport = commands[4]
+
+            if not self.director.enable(protocol,
+                                        host, port,
+                                        vip, vport):
+                logger.error("Could not enable %s" % host)
+        else:
+            print syntax
+
+    def complete_disable(self, text, line, begidx, endidx):
+        """Tab completion for the disable command"""
+        tokens = line.split()
+        if len(line) < 12:
+            completions = [p for p in self.protocols if p.startswith(text)]
+        elif len(tokens) == 2:
+            prot = tokens[1]
+            reals = self.director.get_real(prot)
+            if not text:
+                completions = reals[:]
+        elif len(tokens) == 3 and text:
+            prot = tokens[1]
+            reals = self.director.get_real(prot)
+            completions = [p for p in reals if p.startswith(text)]
+        elif len(tokens) == 4 and not text:
+            prot = tokens[1]
+            virtuals = self.director.get_virtual(prot)
+            completions = virtuals[:]
+        elif len(tokens) == 5 and text:
+            virtuals = self.director.get_virtual(prot)
+            completions = [p for p in virtuals if p.startswith(text)]
+        else:
+            completions = list()
+
+        return completions
+
+    def complete_enable(self, text, line, begidx, endidx):
+        """Tab completion for the disable command"""
+        tokens = line.split()
+        if len(line) < 11:
+            completions = [p for p in self.protocols if p.startswith(text)]
+        elif len(tokens) == 2 and not text:
+            prot = tokens[1]
+            reals = self.director.get_real(prot)
+            completions = reals[:]
+        elif len(tokens) == 3 and text:
+            prot = tokens[1]
+            reals = self.director.get_real(prot)
+            completions = [p for p in reals if p.startswith(text)]
+        elif len(tokens) == 4 and not text:
+            prot = tokens[1]
+            virtuals = self.director.get_virtual(prot)
+            completions = virtuals[:]
+        elif len(tokens) == 5 and text:
+            prot = tokens[1]
+            virtuals = self.director.get_virtual(prot)
+            completions = [p for p in virtuals if p.startswith(text)]
+        else:
+            completions = list()
+
+        return completions
--- lvsm/modules/ldirectordprompts.py
+++ lvsm/modules/ldirectordprompts.py
@@ -62,3 +62,89 @@ class VirtualPrompt(shell.VirtualPrompt):
     #             logger.error("Could not enable %s" % host)
     #     else:
     #         print syntax
+
+class RealPrompt(shell.RealPrompt):
+    def __init__(self, config, rawprompt='', stdin=sys.stdin, stdout=sys.stdout):
+        shell.RealPrompt.__init__(self, config)
+
+    def do_disable(self, line):
+        """
+        \rDisable real server across VIPs.
+        \rsyntax: disable <rip> <port>
+        """
+
+        syntax = "*** Syntax: disable <rip> <port>"
+
+        commands = line.split()
+        if len(commands) > 2 or len(commands) == 0:
+            print syntax
+        elif len(commands) <= 2:
+            host = commands[0]
+            if len(commands) == 1:
+                port = ''
+            elif len(commands) == 2:
+                port = commands[1]
+            else:
+                print syntax
+                return
+            # ask for an optional reason for disabling
+            reason = raw_input("Reason for disabling [default = None]: ")
+            if not self.director.disable(host, port, reason=reason):
+                logger.error("Could not disable %s" % host)
+        else:
+            print syntax
+
+    def do_enable(self, line):
+        """
+        \rEnable real server across VIPs.
+        \rsyntax: enable <rip> <port>
+        """
+
+        syntax = "*** Syntax: enable <rip> <port>"
+
+        commands = line.split()
+        if len(commands) > 2 or len(commands) == 0:
+            print syntax
+        elif len(commands) <= 2:
+            host = commands[0]
+            if len(commands) == 1:
+                port = ''
+            elif len(commands) == 2:
+                port = commands[1]
+            else:
+                print syntax
+                return
+            if not self.director.enable(host, port):
+                logger.error("Could not enable %s" % host)
+        else:
+            print syntax
+
+    def complete_disable(self, text, line, begidx, endidx):
+        """Tab completion for disable command"""
+        tokens = line.split()
+        reals = self.director.get_real(protocol='')
+
+        if not text:
+            if len(tokens) == 1:
+                completions = reals[:]
+        elif len(tokens) == 2:
+            completions = [r for r in reals if r.startswith(text)]
+        else:
+            completions = list()
+
+        return completions
+
+    def complete_enable(self, text, line, begidx, endidx):
+        """Tab completion for enable command"""
+        tokens = line.split()
+        reals = self.director.get_real(protocol='')
+
+        if not text:
+            if len(tokens) == 1:
+                completions = reals[:]
+        elif len(tokens) == 2:
+            completions = [r for r in reals if r.startswith(text)]
+        else:
+            completions = list()
+
+        return completions
