e95413299c61af425ba7d819ac3cf7d52b8875a0
Khosrow E <khosrow@khosrow.ca>
2014-03-13 02:46:38 +0000

    changed factory class. Now only loads modules on demand

--- lvsm/lvs.py
+++ lvsm/lvs.py
@@ -1,17 +1,17 @@
 """
-Director specific funcationality
+Director specific functionality
 """
-from genericdirector import GenericDirector
-from modules.ldirectord import Ldirectord
-from modules.keepalived import Keepalived
+# from genericdirector import GenericDirector
+# from modules.ldirectord import Ldirectord
+# from modules.keepalived import Keepalived
 
 class Director(object):
     """
     Factory class that returns a director object based on the name provided
     """
-    directors = {'generic': GenericDirector,
-                 'ldirectord': Ldirectord,
-                 'keepalived': Keepalived}
+    # directors = {'generic': GenericDirector,
+    #              'ldirectord': Ldirectord,
+    #              'keepalived': Keepalived}
 
     def __new__(self, name, ipvsadm, configfile='', restart_cmd='', nodes='', args=dict()):
         if name == 'keepalived':
